6a049613c8bc9f5eaf164c0ba7b46049cc422383
iamsaintAdrian <adriankimani5@gmail.com>
2024-05-09 14:19:45 +0300

    MADE THE SAVE TAB WORK

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ if(fromLocalStorage){
     render(myLeads)
 }
 tabBtn.addEventListener("click",function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
+    })
+    
 })
 
 
