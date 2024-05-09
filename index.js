870d0fa61e2f309d18e31277e1079477faaac55d
iamsaintAdrian <adriankimani5@gmail.com>
2024-05-09 14:03:47 +0300

    SAVE TAB BUTTON DONE

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const fromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const deleteBtn =document.getElementById("delete-btn")
-
+const tabBtn = document.getElementById("tab-btn")
 if(fromLocalStorage){
     myLeads = fromLocalStorage
     render(myLeads)
 }
+tabBtn.addEventListener("click",function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+})
+
 
 function render (leads){
    let  listItems = " "
